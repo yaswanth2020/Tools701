67d9fae32c5c896d90cc554ca7dff01ff850ead1
racha <racha@RYaswanthReddy>
2025-05-02 08:34:26 +0530

    reviewed

--- Git_GitHubProj01/src/main/java/com/nt/service/UpiPayment.java
+++ Git_GitHubProj01/src/main/java/com/nt/service/UpiPayment.java
@@ -2,6 +2,7 @@ package com.nt.service;
 
 public class UpiPayment
 {
+	//This indicates the single line comment
 	public String msg(String upi)
 	{
 		return "Payment completed through"+upi+"number";
